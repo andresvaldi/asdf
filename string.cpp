e1dea5a4e8592f9f6cf2dc77d9e65afe69edb32c
andresvaldi <andres-tyup@hotmail.com>
2015-10-27 19:22:51 -0600

    stiring.cpp

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+# include <string>
+using namespace std;
+int main(){
+  string h = "hola";
+  string m = ", mundo";
+  cout << h << endl;
+  cout << h.size() << endl;
+  cout << h + m << endl;
+  cout << h.find("o") << endl;
+return 0;
+}
